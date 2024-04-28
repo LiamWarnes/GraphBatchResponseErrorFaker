108bf540867062387c4a213965e02204114319b8
Liam Warnes <Liam.warnes@oneplacesolutions.com>
2024-04-29 09:40:40 +1000

    fix

--- GraphBatchResponseErrorFaker/Program.cs
+++ GraphBatchResponseErrorFaker/Program.cs
@@ -1,13 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GraphBatchResponseErrorFaker;
-using System.Linq;
 using System.Net;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 Console.WriteLine("Input graph batch request body file path:");
 
-string filePath = Console.ReadLine();
+string? filePath = Console.ReadLine();
 
 if (string.IsNullOrEmpty(filePath))
 {
@@ -16,47 +14,54 @@ if (string.IsNullOrEmpty(filePath))
 
 Console.WriteLine("Input combination of status codes delimited by ;");
 
-string statusCodes = Console.ReadLine();
+string? statusCodes = Console.ReadLine();
+
+if (string.IsNullOrEmpty(statusCodes))
+{
+    throw new ArgumentNullException(nameof(statusCodes));
+}
 
 var statusCodesArray = statusCodes.Split(';', StringSplitOptions.RemoveEmptyEntries);
 
-IEnumerable<HttpStatusCode> httpStatusCodes = statusCodesArray.Select(stringCode => (HttpStatusCode)(Convert.ToInt16(stringCode));
+List<HttpStatusCode> httpStatusCodes = statusCodesArray.Select(stringCode => (HttpStatusCode)Convert.ToInt16(stringCode)).ToList();
 
-using (var fileStream = File.OpenRead(filePath))
+using var fileStream = File.OpenRead(filePath);
+
+JsonSerializerOptions options = new JsonSerializerOptions
 {
-    BatchRequestBody body = JsonSerializer.Deserialize(fileStream, typeof(BatchRequestBody)) as BatchRequestBody;
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    WriteIndented = true,
+    DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
 
-    string outputFilePath = $"{filePath}-output.json";
-    if (File.Exists($"{filePath}-output.json"))
-    {
-        File.Delete($"{filePath}-output.json");
-    }
+};
 
-    using (StreamWriter sw = new StreamWriter(outputFilePath, false))
-    {
-        BatchResponseBody batchResponseBody = new BatchResponseBody();
-        Random randomGen = new Random();
-        int codeCount = httpStatusCodes.Count();
-        foreach (var request in body.Requests)
-        {
-            int statusIndex = randomGen.Next(0, codeCount - 1);
-            batchResponseBody.Responses.Add(new Response
-            {
-                Id = request.Id,
-                StatusCode = httpStatusCodes.ElementAt(statusIndex),
-                //Body =
-                //{
-                //    // Could add some error detail here if required
-                //}
-            });
-        }
+BatchRequestBody body = JsonSerializer.Deserialize(fileStream, typeof(BatchRequestBody), options) as BatchRequestBody;
 
-        string responseBody = JsonSerializer.Serialize(batchResponseBody);
-
-        sw.Write(responseBody);
-        sw.Flush();
-    }
+string outputFilePath = $"{filePath}-output.json";
+if (File.Exists($"{filePath}-output.json"))
+{
+    File.Delete($"{filePath}-output.json");
 }
 
+using StreamWriter sw = new StreamWriter(outputFilePath, false);
+BatchResponseBody batchResponseBody = new BatchResponseBody();
+Random randomGen = new Random();
+int codeCount = httpStatusCodes.Count();
+foreach (var request in body.Requests)
+{
+    int statusIndex = randomGen.Next(0, codeCount - 1);
+    batchResponseBody.Responses.Add(new Response
+    {
+        Id = request.Id,
+        StatusCode = httpStatusCodes.ElementAt(statusIndex),
+        //Body =
+        //{
+        //    // Could add some error detail here if required
+        //}
+    });
+}
 
+string responseBody = JsonSerializer.Serialize(batchResponseBody, typeof(BatchResponseBody), options);
 
+sw.Write(responseBody);
+sw.Flush();
